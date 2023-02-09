d5f4858a44a2f0bfecf97eec1df77b1a95dfcf04
edatkinson <98823225+edatkinson@users.noreply.github.com>
2023-02-09 13:12:14 +0000

    Add files via upload

--- ODEsolver.m
+++ ODEsolver.m
@@ -1,14 +1,12 @@
 
-
-
 %initial conditions
-theta_0 = [0;pi];
+theta_0 = [0;0.01];
 
-r_ac = 0.1017;
+r_ac = 0.1017;%reason is the same for I_c
 g = 9.81;
 m = 0.2159;
-I_c = 9.47e-4;
-mu= 1;
+I_c = 9.47e-4; %still need to get accurate values for R1,R2,R3 before this is accurate
+mu= 0.8;
 
 %Time point
 t_span = [0,5];
@@ -16,18 +14,36 @@ t_span = [0,5];
 %solve ODE;
 [t,theta_results] = ode45(@(t, theta) ode_func(t, theta,g,r_ac,m,I_c,mu),t_span,theta_0);
 
+
+angular_vel = diff(theta_results(:,1));
+angular_acc = diff(angular_vel);
+t1 = linspace(0,5,188);%needed for the new angular_vel vector as its smaller than t
+t2 = linspace(0,5,187);%same reason
+
+
 figure(1)
-tiledlayout(2,1)
+tiledlayout(4,1)
 
 nexttile
-plot(t,theta_results(:,1),"b", 'LineWidth',2)
-ylabel('Angular Velocity $\dot{\theta}$',Interpreter='latex')
+plot(t,theta_results(:,1),"b", 'LineWidth',2) %plots the Angular Displacement as a function of time
+ylabel('Angular Displacement ${\theta}$',Interpreter='latex')
 xlabel('Time (s)')
 grid on
 
 nexttile
-plot(t,theta_results(:,2),"r","LineWidth",2)
-ylabel('Displacement of the centroid in the y axis (m)',Interpreter='latex')
+plot(t,theta_results(:,2),"r","LineWidth",2)%Plots the displacement as a function of time
+ylabel('Displacement ',Interpreter='latex')
 xlabel('Time (s)')
 grid on
 
+nexttile
+plot(t1,angular_vel,"g","LineWidth",2)
+ylabel('Angular Velocity ${\dot\theta}$',Interpreter='latex')
+xlabel('Time(s)')
+grid on
+
+nexttile
+plot(t2,angular_acc,"m","LineWidth",2)
+ylabel('Angular Acceleration ${\ddot\theta}$',Interpreter='latex')
+xlabel('Time(s)')
+grid on
