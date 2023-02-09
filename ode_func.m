fa7f304eebba4722fb7817f4dcc48a194a8af121
edatkinson <98823225+edatkinson@users.noreply.github.com>
2023-02-09 13:13:33 +0000

    Add files via upload

--- ode_func.m
+++ ode_func.m
@@ -1,8 +1,11 @@
 
 function [dtheta_dt] = ode_func(t,theta,g,r_ac,m,I_c,mu)
-    theta1 = theta(1);
-    theta2 = theta(2);
-    dtheta1_dt = theta2;
-    dtheta2_dt = -mu*theta2 + ((m*g/r_ac)*sin(theta1))/(m + (I_c/r_ac^2));
-    dtheta_dt = [dtheta1_dt; dtheta2_dt];
+    theta1 = theta(1);%this is essentially theta
+    theta2 = theta(2);%this is essentially theta double dot.
+    dtheta1_dt = theta2;%splitting the ODE into a vector form of 2 ODEs.
+    dtheta2_dt = (-mu*theta2)/(m+(I_c/(r_ac^2))) + ((m*g/r_ac)*sin(theta1))/(m + (I_c/r_ac^2));
+    %-mu*theta2 is the damping component for our system, if we didnt have
+    %this the curve repeat periodically with same amplitude.
+    dtheta_dt = [dtheta1_dt; dtheta2_dt];%vector of the 2 ODEs
 end
+
