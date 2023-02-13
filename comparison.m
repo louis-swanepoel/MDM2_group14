5f7fd1ae6dc2a99c6a981976897b5f0ca7a42217
Andrei <vn21772@bristol.ac.uk>
2023-02-13 14:49:32 +0000

    change

--- comparison.m
+++ comparison.m
@@ -1,10 +1,12 @@
 thetaexp=theta;
-thetamath=theta_results(:,1);
+thetamath=theta_results(:,1,:);
 thetamath=linspace(thetamath(1),thetamath(end),numel(thetaexp));
 
-for i=1:size(thetaexp)
-    diff(i)=abs(thetaexp(i)-thetamath(i))
+for j=1:numel(thetaexp)
+    for i=1:numel(thetaexp)
+        obj(i)=abs(thetaexp(i)-thetamath(i,1,j))
+    end
 end
 
-mu=linspace(0,1,numel(thetaexp))
-plot(mu,diff)
+mu=linspace(0,1,numel(thetaexp));
+plot(mu,obj);
