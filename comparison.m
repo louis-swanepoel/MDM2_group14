c7460ac9010217d41898a857a362dba4a2a3559d
Edward Atkinson <edatkinson@eduroam-11-59.nomadic.bris.ac.uk>
2023-02-13 15:08:17 +0000

    add

--- comparison.m
+++ comparison.m
@@ -1,12 +1,21 @@
 thetaexp=theta;
 thetamath=theta_results(:,1,:);
-thetamath=linspace(thetamath(1),thetamath(end),numel(thetaexp));
-
+%thetamath=linspace(thetamath(1),thetamath(end),numel(thetaexp));
 for j=1:numel(thetaexp)
     for i=1:numel(thetaexp)
-        obj(i)=abs(thetaexp(i)-thetamath(i,1,j))
+        obj(j,i)=abs(thetaexp(i)-theta_results(i,1,j));
     end
 end
+obj(isnan(obj))= 0;
+for i=1:height(obj)
+    newobj(i) = sum(obj(i,:))
+end
+
+
 
 mu=linspace(0,1,numel(thetaexp));
-plot(mu,obj);
+plot(mu,newobj);
+%xlim([])
+ylim([100 200])
+
+
