14141b28b1962d8c8cb8ef4cc0915fe0c9bad27c
lswanepoel <98822407+louis-swanepoel@users.noreply.github.com>
2023-02-13 13:06:54 +0000

    add

--- CentreOfMass.m
+++ CentreOfMass.m
@@ -1,23 +1,20 @@
 function [Centroid] = CentreOfMass(image)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
 
 originalImage = imread(image);
 
 % Clean image of artifacts
-cleanImage =  bwareaopen(originalImage, 150);
+cleanImage =  bwareaopen(originalImage, 20);
 cleanImage = imfill(cleanImage, 'holes');
 
 
 if cleanImage == 0
-    Centroid = NaN;
+    Centroid = nan;
     return
 else
     % label the sphere and collect the data for the sphere 
     [labeledImage, ~] = bwlabel(cleanImage, 8);
     props = regionprops(labeledImage, originalImage, 'all');
     Centroid = props(1).Centroid;
+    return
 
-%figure(6)
-%imshow(cleanImage)
 end
