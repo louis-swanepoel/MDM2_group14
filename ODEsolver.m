98f7c34f552ce9a8a5892eb9d59042e020d7fb32
edatkinson <98823225+edatkinson@users.noreply.github.com>
2023-02-22 22:10:51 +0000

    Add files via upload

--- ODEsolver.m
+++ ODEsolver.m
@@ -5,9 +5,10 @@ r_ac = 0.054188;
 g = 9.81;
 m = 0.2159;
 I_c = 6.577e-3;
-mu1= linspace(0,1,172);
-mu2 = 1;%could be 0.847
-mu2 = 0.9;
+mu1= linspace(0.0125,1,172);
+mu2 = 1.5;%could be 0.847
+mu3 = 0.845;
+%mu2 = 0.9;
 %Time point
 t_span = linspace(0,20,172);
 t_experiment = linspace(0,6,172);
@@ -24,46 +25,43 @@ for j = k
          obj(j,i) = abs(theta_results(i,1,j) - theta(i));
     end
 end
-git 
-
-new = sum(obj(:,:));
 
 
-% for i = k
-%     new(i) = sum(obj(i,:)); %summing all rows in obj
-% end
+new = zeros(size(obj, 1), 1);
+for i = 1:size(obj, 1)
+  new(i) = sum(obj(i,:)); 
+end
+
+
+[t,theta_results2] = ode45(@(t, tHeta) ode_func(t,tHeta,g,r_ac,m,I_c,mu3),t_experiment,theta_0);
 
 new2 = sum(obj(1:end,:));
 
-[t,theta_results2] = ode45(@(t, tHeta) ode_func(t,tHeta,g,r_ac,m,I_c,mu2),t_experiment,theta_0);
-
-
 figure(1)
-%tiledlayout(2,1)
 
-
-
-% tiledlayout(3,1)
+%tiledlayout(3,1)
 % 
-% nexttile
-% plot(t,theta_results2(:,1),"b", 'LineWidth',2) %plots the Angular Displacement as a function of time
-% hold on
-% plot(t_experiment,theta,'m', 'LineWidth',2)
-% ylabel('Angular Displacement ${\theta}$',Interpreter='latex',FontSize=18)
-% legend('${\theta_{an}}$', '$\theta_{exp}$',Interpreter='latex',FontSize=18)
-% xlabel('Time (s)',Interpreter='latex',FontSize=18)
-% grid on
-%figure(2)
 %nexttile
-%plot(t,theta_results2(:,2),"b","LineWidth",2)%Plots the Angular Velocity as a function of time
-%hold on
-%plot(t_experiment,thetaDot,'m',"Linewidth",2)
-%ylabel('Angular Velocity ${\dot\theta}$',Interpreter='latex')
-%legend('Analytical Model', 'Experimental Model')
-%xlabel('Time (s)',Interpreter='latex')
-%grid on
-%figure(3)
+plot(t,theta_results2(:,1),"b", 'LineWidth',2) %plots the Angular Displacement as a function of time
+hold on
+plot(t_experiment,theta,'m', 'LineWidth',2)
+ylabel('Angular Displacement ${\theta}$',Interpreter='latex',FontSize=18)
+legend('${\theta_{an}}$', '$\theta_{exp}$',Interpreter='latex',FontSize=18)
+xlabel('Time (s)',Interpreter='latex',FontSize=18)
+grid on
+
+figure(2)
+%nexttile
+plot(t,theta_results2(:,2),"b","LineWidth",2)%Plots the Angular Velocity as a function of time
+hold on
+plot(t_experiment,thetaDot,'m',"Linewidth",2)
+ylabel('Angular Velocity ${\dot\theta}$',Interpreter='latex')
+legend('Analytical Model', 'Experimental Model')
+xlabel('Time (s)',Interpreter='latex')
+grid on
+figure(3)
 % nexttile
+
 
 plot(mu1, new2,"k","LineWidth",2)
 grid on
@@ -72,66 +70,6 @@ xlabel('Friction Coefficient ${\mu}$',Interpreter='latex',FontSize=18)
 legend('${\phi}$(${\mu}$)',Interpreter='latex',Fontsize=18)
 
 
-% fig=openfig("ComparisonOfTheta.fig",'new','invisible');
-% saveas(fig,"ComparisonOfTheta.jpg",'jpg')
-
-[t,theta_results2] = ode45(@(t, tHeta) ode_func(t,tHeta,g,r_ac,m,I_c,mu2),t_span,theta_0);
-
-
-%figure(1)
-% tiledlayout(3,1)
-% 
-% nexttile
-%plot(t,theta_results2(:,1),"b", 'LineWidth',2) %plots the Angular Displacement as a function of time
-%hold on
-%plot(t_experiment,theta,'m', 'LineWidth',2)
-%ylabel('Angular Displacement ${\theta}$',Interpreter='latex')
-%legend('Analytical Model', 'Experimental Model')
-%xlabel('Time (s)',Interpreter='latex')
-%grid on
-%figure(2)
-%nexttile
-%plot(t,theta_results2(:,2),"b","LineWidth",2)%Plots the Angular Velocity as a function of time
-%hold on
-%plot(t_experiment,thetaDot,'m',"Linewidth",2)
-%ylabel('Angular Velocity ${\dot\theta}$',Interpreter='latex')
-%legend('Analytical Model', 'Experimental Model')
-%xlabel('Time (s)',Interpreter='latex')
-%grid on
-%figure(3)
-% nexttile
-% plot(mu, new,"k","LineWidth",2)
-% grid on
-% ylabel('Objective Function ${\phi}$(${\mu}$)',Interpreter='latex')
-% xlabel('Friction Coefficient ${\mu}$',Interpreter='latex')
-
-% fig=openfig("ANALYTICAL_VELOCity.fig",'new','invisible');
-% saveas(fig,"ANALYTICAL_VELOCity.jpg",'jpg')
-% close(fig);
-% 
-% fig=openfig("Angular_Velocity.fig",'new','invisible');
-% saveas(fig,"Angular_Velocity.jpg",'jpg')
-
-% close(fig);
-% 
-% fig=openfig("AngularDisplacement.fig",'new','invisible');
-% saveas(fig,"AngularDisplacement.jpg",'jpg')
-% close(fig);
-% 
-% fig=openfig("Disp_Vel_Fric.fig",'new','invisible');
-% saveas(fig,"Disp_Vel_Fric.jpg",'jpg')
-% close(fig);
-
-
-fig=openfig("New_FrictionGraph.fig",'new','invisible');
-saveas(fig,"New_FrictionGraph.jpg",'jpg')
-close(fig);
-
-% 
-% fig=openfig("Friction_Coefficient.fig",'new','invisible');
-% saveas(fig,"Friction_Coefficient.jpg",'jpg')
-% close(fig);
-gi 
 
 
 
